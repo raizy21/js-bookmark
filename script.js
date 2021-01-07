32a1820ddc9760d67ded086d06b0e3231d8da613
raizy21 <chiper_raizy@yahoo.com>
2021-01-07 09:46:23 +0100

    init_bookmark

---
+++ script.js
@@ -0,0 +1,15 @@
+/********************
+ * links
+ *
+ * {@link} - https://www.heropatterns.com/
+ * {@link} - https://fonts.google.com/
+ * {@link} - https://www.w3schools.com/cssref/pr_pos_vertical-align.asp
+ * {@link} - https://fontawesome.com/icons?d=gallery
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/HTML/Element/label
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event
+ * {@link} - https://www.w3schools.com/jsref/event_preventdefault.asp
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+ * {@link} - https://regexr.com/
+ * {@link} - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+ **********************/
